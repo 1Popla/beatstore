ff9063bf947dcd844edeb5ed84d458b5e6b3c303
Marcin Poplawski <marcin.poplawski988@gmail.com>
2022-10-21 20:05:16 +0200

    Privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post "cart/add"
   post "cart/remove"
   get "search", to: "beats#search"
+  get "privacy", to: "beats#privacy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
